40bc992c045a5fd9ade357927b37f42167af939d
enriqueescobar-askida <enrique.escobar@askida.com>
2019-02-28 16:20:28 -0500

    baseline

---
+++ Console/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Console
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Amaris.Foundation.CrowdAct.NetDal.Contexts;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CrowdActDbContext crowdActDbContext = new CrowdActDbContext();
+        }
+    }
+}
